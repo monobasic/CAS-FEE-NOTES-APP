c3ee65fd90453580bffd6557f5ed8aecfcad367b
André Abt <info@andre-abt.com>
2017-09-24 17:23:07 +0200

    Added demo code

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,11 @@
 // app.js
 console.log('hello world from app.js');
+
+import $ from 'jquery';
+import moment from 'moment';
+
+$(function() {
+  $('body').css('background', 'hotpink');
+});
+
+console.log(moment().format());
