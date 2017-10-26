4c3d56402708d62acaaa6f2c9203cd14b53ae18a
André Abt <info@andre-abt.com>
2017-10-26 14:55:31 +0200

    Added new dataservice

---
+++ src/js/modules/DataServiceRest.js
@@ -0,0 +1,29 @@
+import DataServiceAbstract from './DataServiceAbstract';
+
+export default class DataServiceRest extends DataServiceAbstract {
+
+  constructor() {
+    super();
+
+  }
+
+  getNotes() {
+
+  }
+
+  getNote(id) {
+
+  }
+
+  addNote(note) {
+
+  }
+
+  deleteNote(id) {
+
+  }
+
+  updateNote(id, data) {
+
+  }
+}
