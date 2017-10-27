2a6db51b42963c10d1729deb67f1f73bbcbdf047
André Abt <info@andre-abt.com>
2017-10-27 16:20:14 +0200

    Removed unused method override

--- server/index.js
+++ server/index.js
@@ -10,16 +10,8 @@ const allowCrossDomain = function(req, res, next) {
   next();
 };
 app.use(allowCrossDomain);
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(require("method-override")(function(req, res) {
-  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    let method = req.body._method;
-    delete req.body._method;
-    return method;
-  }
-}));
 
 app.use(require('./routes/notesRoutes.js'));
 
