56d52299aa79edc8429f40da2b0aa2293f6e891c
André Abt <info@andre-abt.com>
2017-11-01 13:16:52 +0100

    Removed nonsense test

--- spec/NoteModelSpec.js
+++ spec/NoteModelSpec.js
@@ -27,12 +27,6 @@ describe("Note Model getNotes() Tests", function() {
     expect(dataServiceFake.getNotes).toHaveBeenCalled();
   });
 
-  it("On getNotes(), default attribues should be 'due' and 'false'", function() {
-    spyOn(noteModel, 'getNotes');
-    noteModel.getNotes('due', false);
-    expect(noteModel.getNotes).toHaveBeenCalledWith('due', false);
-  });
-
   it("On getNotes('due', false), _sortyBy should have been called but not __filterFinished", function() {
     spyOn(noteModel, '_sortBy');
     spyOn(noteModel, '_filterFinished');
@@ -50,7 +44,5 @@ describe("Note Model getNotes() Tests", function() {
       expect(noteModel._filterFinished).toHaveBeenCalled();
     });
   });
-
-
-
+  
 });
