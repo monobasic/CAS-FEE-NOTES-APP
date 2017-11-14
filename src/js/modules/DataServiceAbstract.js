2cdb72d6bbc53eb5aec4549ab9c2401c9266cda1
André Abt <info@andre-abt.com>
2017-11-14 22:38:27 +0100

    Abstract more DRY

--- src/js/modules/DataServiceAbstract.js
+++ src/js/modules/DataServiceAbstract.js
@@ -5,20 +5,18 @@ export default class DataServiceAbstract {
       throw new TypeError('Abstract class "DataServiceAbstract" cannot be instantiated directly.');
     }
 
-    if (this.getNotes === undefined) {
-      throw new TypeError('You have to implement the method getNotes()');
-    }
-    if (this.getNote === undefined) {
-      throw new TypeError('You have to implement the method getNote()');
-    }
-    if (this.addNote === undefined) {
-      throw new Error('You have to implement the method addNote()');
-    }
-    if (this.deleteNote === undefined) {
-      throw new Error('You have to implement the method deleteNote()');
-    }
-    if (this.updateNote === undefined) {
-      throw new Error('You have to implement the method updateNote()');
-    }
+    const methods = [
+      'getNotes',
+      'getNote',
+      'addNote',
+      'deleteNote',
+      'updateNote'
+    ];
+
+    methods.forEach((method) => {
+      if (this[method] === undefined) {
+        throw new TypeError(`You have to implement the method ${method}()`);
+      }
+    });
   }
 }
